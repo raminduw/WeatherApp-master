6f44166c1a33ab48fd5021f395c22247a4e8c355
Ramindu Weeraman <ramindu.weeraman@gmail.com>
2019-11-12 22:55:01 +0100

    method implemented

--- app/src/main/java/com/ramindu/weeraman/weather/data/DataUtil.java
+++ app/src/main/java/com/ramindu/weeraman/weather/data/DataUtil.java
@@ -1,9 +1,11 @@
 package com.ramindu.weeraman.weather.data;
 
+import android.util.Log;
+
 public class DataUtil {
 
     public void printName(){
-
+        Log.d("TAG" , "Ramindu Nuwan Weeraman");
     }
 
     public void printName(String name){
