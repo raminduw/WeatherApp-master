e73c38ae0b808963a16178ce4f9946ae6689ca71
Ramindu Weeraman <ramindu.weeraman@gmail.com>
2019-11-17 19:50:45 +0100

    rebase 1.

--- app/src/main/java/com/ramindu/weeraman/weather/data/DataUtil.java
+++ app/src/main/java/com/ramindu/weeraman/weather/data/DataUtil.java
@@ -9,7 +9,7 @@ public class DataUtil {
         Log.d("TAG" , "Ramindu Nuwan Weeraman");
         Log.d("TAG" , "Ramindu Nuwan Weeraman");
         Log.d("TAG" , "Ramindu Nuwan Weeraman");
-        Log.d("TAG" , "Ramindu Nuwan Weeraman");
+        Log.d("TAG" , "Ramindu Nuwan Weeraman rebase3");
     }
 
     public void printName(String name){
