c5cd824a76ff762dc661f7be60fbba05d6776ab8
Ramindu Weeraman <ramindu.weeraman@gmail.com>
2020-01-09 20:31:26 +0100

    Squash 6000.

--- app/src/main/java/com/ramindu/weeraman/weather/data/DataUtil.java
+++ app/src/main/java/com/ramindu/weeraman/weather/data/DataUtil.java
@@ -33,6 +33,12 @@ public class DataUtil {
         Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 4");
         Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 5");
         Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 6");
+
+
+        Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 4");
+        Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 5");
+        Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 6");
+        Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 7");
         Log.d("TAG" , "Ramindu Nuwan Weeraman testSqash 7");
 
 
